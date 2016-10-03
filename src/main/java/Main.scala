9259d6a50cc170ad27dea7a02f85aa1753395b88
Sergio <sergioifg94@gmail.com>
2016-10-03 22:16:05 +0200

    Implemented input filename passing as argument

--- src/main/java/Main.scala
+++ src/main/java/Main.scala
@@ -3,11 +3,25 @@ import CodeGen.Javascript.{JavascriptCode, JavascriptCodeES5, JavascriptCodeES6}
 import Naming.NodeNamingImpl
 import Parse.Impl.HtmlParserImpl
 
+import scala.io.Source
+
 object Main {
 
   def main(args: Array[String]): Unit = {
+    val input = getInputText(getInputFile(args))
     val jsCode = getEcmascript(args)
 
+    // If no input could be open, show a message and finish
+    if (input.isEmpty) {
+      println(
+        """
+          |No input file
+        """.stripMargin
+      )
+      return
+    }
+
+    // If the ES specification was invalid, show a message and finish
     if (jsCode.isEmpty) {
       println(
         """Invalid ECMAScript option. Accepted values are:
@@ -17,12 +31,11 @@ object Main {
       return
     }
 
-
     val parser = new HtmlParserImpl
     val codeGen = new CodeGeneratorImpl(jsCode.get)
     val naming = new NodeNamingImpl
 
-    val tree = parser.parseHtml("""<div id="mydiv" style="background-color: #ccc;"><p data-repeat="[1,2,3,4]">Hello World</p></div>""")
+    val tree = parser.parseHtml(input.get)
 
     tree match {
       case Some(justTree) => println(codeGen.generateCode(naming.nameNodes(justTree)))
@@ -45,9 +58,36 @@ object Main {
         case "es5" => return Some(new JavascriptCodeES5)
         case _     => return None
       }
+
+      case _ => ;
     }
 
     Some(new JavascriptCodeES6)
   }
 
+  /**
+    * Get the input file from the program arguments
+    * @param args Program arguments
+    * @return Name of the input
+    */
+  def getInputFile(args: Array[String]) : Option[String] = {
+    val prevIndex = args.indexWhere(_ == "-i")
+
+    if (prevIndex == -1)
+      None
+    else
+      Some(args(prevIndex + 1))
+  }
+
+  /**
+    * Get the contents of the input file
+    * @param file File name
+    * @return
+    */
+  def getInputText(file: Option[String]) : Option[String] = {
+    for (
+      fileName <- file;
+      file = Source.fromFile(fileName).mkString
+    ) yield file
+  }
 }
