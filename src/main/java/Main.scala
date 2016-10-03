31f3a5c31a87ca24a83a3085fb07e50cf8389296
Sergio <sergioifg94@gmail.com>
2016-10-03 21:33:29 +0200

    JS Specification selected by program arguments

--- src/main/java/Main.scala
+++ src/main/java/Main.scala
@@ -1,15 +1,25 @@
 import CodeGen.CodeGeneratorImpl
-import CodeGen.Javascript.JavascriptCodeES6
+import CodeGen.Javascript.{JavascriptCode, JavascriptCodeES5, JavascriptCodeES6}
 import Naming.NodeNamingImpl
 import Parse.Impl.HtmlParserImpl
 
 object Main {
 
   def main(args: Array[String]): Unit = {
-    val jsCode = new JavascriptCodeES6
+    val jsCode = getEcmascript(args)
+
+    if (jsCode.isEmpty) {
+      println(
+        """Invalid ECMAScript option. Accepted values are:
+          | es6
+          | es5
+        """.stripMargin)
+      return
+    }
+
 
     val parser = new HtmlParserImpl
-    val codeGen = new CodeGeneratorImpl(jsCode)
+    val codeGen = new CodeGeneratorImpl(jsCode.get)
     val naming = new NodeNamingImpl
 
     val tree = parser.parseHtml("""<div id="mydiv" style="background-color: #ccc;"><p data-repeat="[1,2,3,4]">Hello World</p></div>""")
@@ -18,6 +28,26 @@ object Main {
       case Some(justTree) => println(codeGen.generateCode(naming.nameNodes(justTree)))
       case _ => println("Failed parsing input")
     }
+  }
+
+  /**
+    * Get the JavascriptCode implementation given the program arguments
+    * @param args Program arguments
+    * @return Implementation of JavascriptCode. If the value is wrong, returns None. If the argument is not passed,
+    *         returns the default implementation
+    */
+  def getEcmascript(args: Array[String]) : Option[JavascriptCode] = {
+    val pattern = "--ecmascript=([a-zA-Z0-9]+)".r
+
+    args.foreach {
+      case pattern(version) => version match {
+        case "es6" => return Some(new JavascriptCodeES6)
+        case "es5" => return Some(new JavascriptCodeES5)
+        case _     => return None
+      }
+    }
+
+    Some(new JavascriptCodeES6)
   }
 
 }
