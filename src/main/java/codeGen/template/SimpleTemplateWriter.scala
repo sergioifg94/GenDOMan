5dba6d57cc032afb594ce151674cf7d35236034d
Sergio <sergioifg94@gmail.com>
2016-10-24 19:10:13 +0200

    Changed template code generation to use different token

--- src/main/java/codeGen/template/SimpleTemplateWriter.scala
+++ src/main/java/codeGen/template/SimpleTemplateWriter.scala
@@ -2,14 +2,14 @@ package codeGen.template
 
 class SimpleTemplateWriter extends TemplateWriter {
 
-  val REGEX = """\$\{code\}"""
+  val REPLACE_TOKEN = """{{code}}"""
 
   /**
-    * Writes a string into a template by regex substitution
+    * Writes a string into a template by string substitution
     *
     * @param template Template
     * @param contents Contents to be written in the template
     */
-  override def write(template: String, contents: String): String = template.replaceAll(REGEX, contents)
+  override def write(template: String, contents: String): String = template.replaceAllLiterally(REPLACE_TOKEN, contents)
 
 }
